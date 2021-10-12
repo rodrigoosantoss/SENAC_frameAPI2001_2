bf9b0927f46f710d6b5589649fef4a8bc31adfb0
rodrigo@terra.com.br <rodrigo@terra.com.br>
2021-10-12 14:14:33 -0300

    Adicionado um template do bootstrap a pagina bemvindo

--- app-login/app/Controllers/Http/LoginController.js
+++ app-login/app/Controllers/Http/LoginController.js
@@ -9,12 +9,14 @@ class LoginController {
         return view.render('login');
     }
     
-    async login({request, response}) {
+    async login({request, response, session}) {
         const usuario = new Usuario();
         usuario.email = request.input('email');
         usuario.senha = request.input('senha');
+        usuario.nome = "Administrador";
             if(usuario.email == "admin@admin.com" && usuario.senha == "12345"){
                 console.log("Congrats LOGADO com sucesso!!")
+                session.put('nome', usuario.nome) 
                 return response.redirect('/bemvindo')
             } else {
             console.log("que coisa, ainda não deu!!")
@@ -23,8 +25,13 @@ class LoginController {
             }
             
 
-    async bemVindo({view}) {
-        return view.render('/bemvindo');
+    async bemVindo({view, session, response}) {
+        const nome = session.get('nome')
+        if(nome){
+            return view.render('/bemvindo', {nome, nome});
+        }else{
+            response.redirect('/')
+        }
 
     }
 }
