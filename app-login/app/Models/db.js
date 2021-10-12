88c821019b7bffe2c52c1f2a1c3f6fe7547e1f5d
rodrigo@terra.com.br <rodrigo@terra.com.br>
2021-10-12 14:15:30 -0300

    adicionado template BootStrap a pagina bemvindo

--- app-login/app/Models/db.js
+++ app-login/app/Models/db.js
@@ -1,10 +1,17 @@
-const Sequelize = require('sequelize')
+const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('app_login.usuarios', 'root', '199730Lu#', {
+const sequelize = new Sequelize('app_login.usuarios', 'root', '19730Lu#', {
     host: 'localhost',
     dialect: 'mysql'
     
 });
+
+sequelize.authenticate()
+.then(function(){
+    console.log("BD (app_login) conectado sucesso!!")
+}).catch(function(){
+    console.log("BD ERRO de conexão!!")
+})
 
 
 module.exports = sequelize;
